d2936f61b2762849e2eed800178603f0451206be
michael-mulligan <31669632+michael-mulligan@users.noreply.github.com>
2018-01-22 18:04:24 -0500

    Added final LINQ Queries

--- Lab1-LINQInvoice/Program.cs
+++ Lab1-LINQInvoice/Program.cs
@@ -26,7 +26,54 @@ namespace Lab1_LINQInvoice
                 orderby element.PartDescription
                 select element;
 
+            Console.WriteLine("a) Sorted by description:\n");
             foreach(var i in sortByDescription)
+            {
+                Console.WriteLine(i);
+            }
+
+            var sortByPrice =
+                from element in invoice
+                orderby element.Price
+
+                select element;
+
+            Console.WriteLine("\nb) Sorted by price:\n");
+            foreach (var i in sortByPrice)
+            {
+                Console.WriteLine(i);
+            }
+
+            var descriptionAndQuantity =
+                from element in invoice
+                orderby element.Quantity
+                select new { PartDescription = element.PartDescription, Quantity = element.Quantity };
+
+            Console.WriteLine("\nc) Description and quantity, sort by quantity:\n");
+            foreach (var i in descriptionAndQuantity)
+            {
+                Console.WriteLine(i);
+            }
+
+            var descriptionAndInvoice =
+                from element in invoice
+                orderby element.Price * element.Quantity
+                select new {PartDescription = element.PartDescription, Total = element.Price * element.Quantity };
+
+            Console.WriteLine("\nd) Description and invoice total, sort by invoice total:\n");
+            foreach (var i in descriptionAndInvoice)
+            {
+                Console.WriteLine(i);
+            }
+
+            var total200_500 =
+                from element in invoice
+                orderby element.Price * element.Quantity
+                where 200 < element.Price * element.Quantity && element.Price * element.Quantity  < 500
+                select new { PartDescription = element.PartDescription, InvoiceTotal = element.Price * element.Quantity };
+
+            Console.WriteLine("\ne) Invoice totals between $200.00 and $500.00:\n");
+            foreach (var i in total200_500)
             {
                 Console.WriteLine(i);
             }

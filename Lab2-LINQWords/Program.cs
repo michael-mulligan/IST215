6fb30be1f013ed1868aeb2f5ff56c4dfe324a63b
michael-mulligan <31669632+michael-mulligan@users.noreply.github.com>
2018-01-22 18:59:59 -0500

    Added Lab 2

---
+++ Lab2-LINQWords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_LINQWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine($"\nYou entered: \n{sentence}\n");
+
+            String[] sentenceArray = sentence.ToLower().Split();
+
+            var sortedWords =
+                from word in sentenceArray
+                orderby word
+                select word;
+
+            Console.Write("Distinct words: ");
+            foreach (var i in sortedWords.Distinct())
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
